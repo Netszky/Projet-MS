200cf648439d8fcfbccea507280db0a0f7b64bef
Rafael METAYER <rafael.metayer@ynov.com>
2020-12-14 14:46:03 +0100

    User API

---
+++ user/src/test/java/ynov/projetms/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package ynov.projetms.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
