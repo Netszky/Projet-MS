641ab9f97025477ae59432c4b8a32bcec97068d8
Netszky <julien.chigot@ynov.com>
2020-12-14 11:15:21 +0100

    sql

--- article/src/main/java/ynov/projetms/article/model/Article.java
+++ article/src/main/java/ynov/projetms/article/model/Article.java
@@ -1,13 +1,24 @@
 package ynov.projetms.article.model;
 
 import java.sql.Date;
+import java.util.Collection;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "articles")
@@ -17,19 +28,25 @@ public class Article {
 	@Column(name="id")
 	private Integer id;
 	
-	@Column(name="date")
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="date_article")
+	@CreationTimestamp
 	private Date date;
 	
 	@Column(name="content")
 	private String content;
 	
-	@Column(name="commentaire")
-	private String commentaire;
+	@OneToMany(mappedBy = "articles", cascade = CascadeType.REMOVE)
+	private Collection<Comment> comments;
 	
-	@Column(name="id_category")
+	@OneToOne
+	@JoinColumn(name="id_category", referencedColumnName = "id_category", nullable = false)
+	@NotNull
 	private Integer id_category;
 	
-	@Column(name="id_user")
+	@ManyToOne
+	@JoinColumn(name="user_id", referencedColumnName = "user_id", nullable = false)
+	@NotNull
 	private Integer id_user;
 
 	public Integer getId() {
@@ -56,14 +73,6 @@ public class Article {
 		this.content = content;
 	}
 
-	public String getCommentaire() {
-		return commentaire;
-	}
-
-	public void setCommentaire(String commentaire) {
-		this.commentaire = commentaire;
-	}
-
 	public Integer getId_category() {
 		return id_category;
 	}
@@ -78,6 +87,13 @@ public class Article {
 
 	public void setId_user(Integer id_user) {
 		this.id_user = id_user;
+	}
+	public Collection<Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(Collection<Comment> comments) {
+		this.comments = comments;
 	}
 	
 }
