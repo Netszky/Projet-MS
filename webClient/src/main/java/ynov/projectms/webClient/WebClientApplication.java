fb329652a624af8249d5bfed4b88a65e809d6eee
Rafael METAYER <rafael.metayer@ynov.com>
2020-12-14 11:18:09 +0100

    WebClient

---
+++ webClient/src/main/java/ynov/projectms/webClient/WebClientApplication.java
@@ -0,0 +1,13 @@
+package ynov.projectms.webClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebClientApplication.class, args);
+	}
+
+}
