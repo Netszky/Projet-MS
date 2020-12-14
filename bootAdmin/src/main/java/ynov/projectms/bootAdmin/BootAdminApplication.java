f71b7bf07930ce7ef0a5817ae973b6051095c3ac
Netszky <julien.chigot@ynov.com>
2020-12-14 12:37:44 +0100

    config

--- bootAdmin/src/main/java/ynov/projectms/bootAdmin/BootAdminApplication.java
+++ bootAdmin/src/main/java/ynov/projectms/bootAdmin/BootAdminApplication.java
@@ -2,8 +2,13 @@ package ynov.projectms.bootAdmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
 public class BootAdminApplication {
 
 	public static void main(String[] args) {
