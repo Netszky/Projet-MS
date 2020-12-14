4c574d9928f28eb74e65bae731118ac31c30f693
Rafael METAYER <rafael.metayer@ynov.com>
2020-12-14 10:48:50 +0100

    BootAdmin

---
+++ bootAdmin/src/test/java/ynov/projectms/bootAdmin/BootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ynov.projectms.bootAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
