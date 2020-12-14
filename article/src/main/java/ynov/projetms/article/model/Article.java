9f4da5540f99d23f40486faabb7ca3bfeb8e9999
Netszky <julien.chigot@ynov.com>
2020-12-14 10:45:07 +0100

    Article Model / Repo / Controller

---
+++ article/src/main/java/ynov/projetms/article/model/Article.java
@@ -0,0 +1,83 @@
+package ynov.projetms.article.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "articles")
+public class Article {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Integer id;
+	
+	@Column(name="date")
+	private Date date;
+	
+	@Column(name="content")
+	private String content;
+	
+	@Column(name="commentaire")
+	private String commentaire;
+	
+	@Column(name="id_category")
+	private Integer id_category;
+	
+	@Column(name="id_user")
+	private Integer id_user;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCommentaire() {
+		return commentaire;
+	}
+
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
+
+	public Integer getId_category() {
+		return id_category;
+	}
+
+	public void setId_category(Integer id_category) {
+		this.id_category = id_category;
+	}
+
+	public Integer getId_user() {
+		return id_user;
+	}
+
+	public void setId_user(Integer id_user) {
+		this.id_user = id_user;
+	}
+	
+}
