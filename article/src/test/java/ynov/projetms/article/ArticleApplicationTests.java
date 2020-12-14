9bf3083db5733acdfe5938cf1abf4123b4cfb56c
Netszky <julien.chigot@ynov.com>
2020-12-14 10:06:59 +0100

    article v2

---
+++ article/src/test/java/ynov/projetms/article/ArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package ynov.projetms.article;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
