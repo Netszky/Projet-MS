b7e258a8f1cdaee014af018ca68c9021745529c4
Netszky <julien.chigot@ynov.com>
2020-12-14 15:32:29 +0100

    config

---
+++ article/src/main/java/ynov/projetms/article/config/CustomProperties.java
@@ -0,0 +1,18 @@
+package ynov.projetms.article.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix= "ynov.projetms")
+public class CustomProperties {
+	private String apiUrl;
+
+	public String getApiUrl() {
+		return apiUrl;
+	}
+
+	public void setApiUrl(String apiUrl) {
+		this.apiUrl = apiUrl;
+	}
+}
--- webClient/src/main/java/ynov/projectms/webClient/model/User.java
+++ webClient/src/main/java/ynov/projectms/webClient/model/User.java
@@ -1,5 +1,45 @@
 package ynov.projectms.webClient.model;
 
 public class User {
+	private Integer id;
+	
+	private String name;
+	
+	private String mail;
+	
+	private String password;
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
