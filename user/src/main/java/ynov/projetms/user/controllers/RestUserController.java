c62d1121dcbd3673a6215fe8fdecc72ea23a199c
Rafael METAYER <rafael.metayer@ynov.com>
2020-12-14 16:13:31 +0100

    User RestUserController Methods

--- user/src/main/java/ynov/projetms/user/controllers/RestUserController.java
+++ user/src/main/java/ynov/projetms/user/controllers/RestUserController.java
@@ -3,8 +3,12 @@ package ynov.projetms.user.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ynov.projetms.user.model.User;
@@ -16,12 +20,45 @@ public class RestUserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@GetMapping("/connect")
+	@GetMapping("/profil/{id}")
 	public Optional<User> getUser(@PathVariable("id") int id) 
 			throws InterruptedException {
-		Optional<User> user = userRepository.findById(id);		
-		System.out.println("Retrieve " + user.get().getName());
+		Optional<User> user = userRepository.findById(id);
 		return user;
+	}
+	
+	@PostMapping("/sign_in")
+	public User createUser(@RequestBody User user) {
+		user = userRepository.save(user);		
+		return user;
+	}
+	
+	@DeleteMapping("/profil/{id}")
+	public void deleteUser(@PathVariable("id") int id) {
+		userRepository.deleteById(id);
+	}
+	
+	@PutMapping("/profil/{id}")
+	public User updateUser(@PathVariable("id") int id, @RequestBody User user) {
+		User currentUser = userRepository.findById(id).get();	
+		
+		String username = user.getName();
+		if(username != null) {
+			currentUser.setName(username);
+		}
+		
+		String mail = user.getMail();
+		if(mail != null) {
+			currentUser.setMail(mail);
+		}
+		
+		String password = user.getPassword();
+		if(password != null) {
+			currentUser.setPassword(password);
+		}
+		
+		currentUser = userRepository.save(currentUser);
+		return currentUser;
 	}
 
 }
