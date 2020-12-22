65df2babb0092ef4525e8aa60644759c08c8b849
Netszky <julien.chigot@ynov.com>
2020-12-22 15:52:51 +0100

    form

--- article/src/main/resources/data.sql
+++ article/src/main/resources/data.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS category;
 CREATE TABLE articles (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   date_article DATE NOT NULL,
+  title VARCHAR(500) NOT NULL,
   content VARCHAR(500) NOT NULL,
   id_category INT NOT NULL,
   id_user INT NOT NULL
@@ -23,7 +24,7 @@ INSERT INTO category (name) VALUES
 ALTER TABLE articles 
 ALTER COLUMN date_article SET DEFAULT CURRENT_TIMESTAMP;
 
-INSERT INTO articles (date_article, content, id_category, id_user) VALUES
-  (CURRENT_TIMESTAMP(),'Test article1', '1','1'),
-  (CURRENT_TIMESTAMP(),'Test article2', '2','2'),
-  (CURRENT_TIMESTAMP(),'Test article3', '3','3');
+INSERT INTO articles (date_article, title, content, id_category, id_user) VALUES
+  (CURRENT_TIMESTAMP(),'Titre1','Test article1', '1','1'),
+  (CURRENT_TIMESTAMP(),'Titre2','Test article2', '2','2'),
+  (CURRENT_TIMESTAMP(),'Titre3','Test article3', '3','3');
